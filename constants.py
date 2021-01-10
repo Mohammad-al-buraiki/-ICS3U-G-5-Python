3e9dc4e06965a25b4716940f4e11627b453489ab
Mohammad Alburaiki <74559902+Mohammad-al-buraiki@users.noreply.github.com>
2021-01-10 02:04:14 -0500

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python3
+
+# Created by: Mohammad-al-buraiki
+# Created on January 2021
+# This program is the space aliens game
+
+# PyBadge screen size is 160*128 and sprites are 16*16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
